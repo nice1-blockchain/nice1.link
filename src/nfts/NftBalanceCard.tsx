efca65a32cbc0b9131fe588289c6147e024ddbf6
AntonioLLopez <proyectos.antonio.lopez@gmail.com>
2023-06-08 11:30:50 +0200

    Added functions for displaying resources

--- src/nfts/NftBalanceCard.tsx
+++ src/nfts/NftBalanceCard.tsx
@@ -5,7 +5,7 @@ import { useNftSimpleAssets } from '../hooks/NftsProvider'
 import DashboardBox from '../components/DashboardBox'
 
 
-export const NftBalance = () => {
+export const NftBalanceCard = () => {
 
   const { nfts } = useNftSimpleAssets()
 
@@ -13,13 +13,12 @@ export const NftBalance = () => {
     <DashboardBox childProps={{ justifyContent: 'center' }} justifyContent='center'>
       <HStack align='stretch'>
         <Box>
-          <Text fontWeight='bold' fontSize='sm' color='gray.400'> NFTs: </Text>
-        </Box>
-        <Box>
-          <Text fontWeight='bold' fontSize='md'>{nfts.length}</Text>
+          <Text alignContent='left' fontSize='md'>NFTs: {nfts.length}</Text>
         </Box>
       </HStack>
     </DashboardBox>
   )
 }
+
+
 
--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -5,13 +5,16 @@ import ProfileCard from '../profile/ProfileCard'
 import PostCard from '../components/PostCard'
 import { NEWS_BASE_URL } from '../constants'
 import { useBlog } from '../hooks/blog'
-import { NftBalance } from '../nfts/NftBalance'
+//import { NftBalanceCard } from '../nfts/NftBalanceCard'
+import { ProfileResourcesCard } from '../profile/ProfileResourcesCard'
+
 
 export default function Dashboard() {
   const { posts, featured } = useBlog()
 
   return (
     <>
+
       <Grid gap={2} templateRows='repeat(1, 1fr)' templateColumns='repeat(4, 1fr)'>
         <GridItem colSpan={{xs: 4, lg: 1}} rowSpan={1}>
           <Grid gap={2} templateColumns='repeat(2, 1fr)'>
@@ -21,12 +24,15 @@ export default function Dashboard() {
             <GridItem colSpan={{xs: 2, md: 1, lg: 2}} display='flex' maxW='100%'>
               <BalanceCard />
             </GridItem>
+            {/* <GridItem colSpan={{ xs: 2, md: 1, lg: 2 }} display='flex' maxW='100%'>
+              <NftBalanceCard />
+            </GridItem> */}
             <GridItem colSpan={{ xs: 2, md: 1, lg: 2 }} display='flex' maxW='100%'>
-              <NftBalance />
+              <ProfileResourcesCard />
             </GridItem>
           </Grid>
         </GridItem>
-        <GridItem colSpan={{xs: 4, lg: 3}} rowSpan={1}>
+        <GridItem colSpan={{xs: 4, lg: 3}} rowSpan={1} >
           <a href={featured?.link}>
             <img src={`${NEWS_BASE_URL}/images/${featured?.image}`} alt='' />
           </a>
