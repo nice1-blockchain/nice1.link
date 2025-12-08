cf1c7f8450c52ad8e09d9afd49f9ea202a9e3099
Alberto R. <alberto@3in-tech.com>
2025-12-08 17:34:59 +0100

    banner centered

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem, Box } from '@chakra-ui/react'
 
 import BalanceCard from '../profile/BalanceCard'
 import ProfileCard from '../profile/ProfileCard'
@@ -15,7 +15,18 @@ export default function Dashboard() {
 
   return (
     <>
+      {/* Banner a ancho completo ARRIBA */}
+      <Box mb={2}>
+        <a href={featured?.link}>
+          <img 
+            src="https://nice1-blockchain.github.io/nice1-link-news//images/first-banner.png" 
+            alt=""
+            style={{ width: '100%', borderRadius: '8px' }}
+          />
+        </a>
+      </Box>
 
+      {/* Grid con ProfileCard, Balance, etc. */}
       <Grid gap={2} templateRows='repeat(1, 1fr)' templateColumns='repeat(4, 1fr)'>
         <GridItem colSpan={{xs: 4, lg: 1}} rowSpan={1}>
           <Grid gap={2} templateColumns='repeat(2, 1fr)'>
@@ -25,22 +36,12 @@ export default function Dashboard() {
             <GridItem colSpan={{xs: 2, md: 1, lg: 2}} display='flex' maxW='100%'>
               <BalanceCard />
             </GridItem>
-            {/* <GridItem colSpan={{ xs: 2, md: 1, lg: 2 }} display='flex' maxW='100%'>
-              <NftBalanceCard />
-            </GridItem>
-            <GridItem colSpan={{ xs: 2, md: 1, lg: 2 }} display='flex' maxW='100%'>
-              <ResourcesCard />
-            </GridItem> */}
             <GridItem colSpan={{ xs: 2, md: 1, lg: 2 }} display='flex' maxW='100%'>
               <ProfileResourcesCard />
             </GridItem>
           </Grid>
         </GridItem>
-        <GridItem colSpan={{xs: 4, lg: 3}} rowSpan={1} >
-          <a href={featured?.link}>
-            <img src={`${NEWS_BASE_URL}/images/${featured?.image}`} alt='' />
-          </a>
-        </GridItem>
+        {/* Aquí puedes poner otro contenido en las 3 columnas restantes si quieres */}
       </Grid>
       <Grid gap={2} templateRows='repeat(1, 1fr)' templateColumns='repeat(4, 1fr)' mt={2}>
         {
